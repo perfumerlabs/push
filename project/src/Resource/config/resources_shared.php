ef8615c9ca3593a33d821e5522ddb3ddd82da957
temirlankasen <temirlankasen@gmail.com>
2021-05-28 17:04:46 +0600

    fix

--- project/src/Resource/config/resources_shared.php
+++ project/src/Resource/config/resources_shared.php
@@ -5,9 +5,9 @@ return [
         'bin'           => 'vendor/bin/propel',
         'project'       => 'push',
         'database'      => 'pgsql',
-        'dsn'           => 'pgsql:host=db;port=5432;dbname=microservices',
-        'db_user'       => 'postgres',
-        'db_password'   => 'root',
+        'dsn'           => 'pgsql:host=PG_HOST;port=PG_PORT;dbname=PG_DATABASE',
+        'db_user'       => 'PG_USER',
+        'db_password'   => 'PG_PASSWORD',
         'platform'      => 'pgsql',
         'config_dir'    => 'src/Resource/propel/connection',
         'schema_dir'    => 'src/Resource/propel/schema',
@@ -21,16 +21,16 @@ return [
     ],
 
     'google' => [
-        'file' => 'google_prod.json',
-        'url'  => 'https://fcm.googleapis.com/v1/projects/myproject-b5ae1/messages:send',
+        'file' => 'GOOGLE_FILE',
+        'url'  => 'GOOGLE_URL',
     ],
     'apple' => [
-        'file' => 'prod1.pem',
-        'url'  => 'https://api.push.apple.com:443/3/device/',
-        'bundle_id' => 'kz.naimi.app',
+        'file' => 'APPLE_FILE',
+        'url'  => 'APPLE_URL',
+        'bundle_id' => 'APPLE_BUNDLE_ID',
     ],
     'huawei' => [
-        'file' => 'huawei_prod.json',
-        'url'  => 'https://push-api.cloud.huawei.com/v1/%s/messages:send',
+        'file' => 'HUAWEI_FILE',
+        'url'  => 'HUAWEI_URL',
     ],
 ];

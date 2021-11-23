70e3f35ffaea094de524ac6bd5e52751e71b3ce2
Temirlan Kassen <temirlankasen@gmail.com>
2021-11-23 13:04:58 +0600

    fix

--- project/src/Resource/config/resources_shared.php
+++ project/src/Resource/config/resources_shared.php
@@ -4,9 +4,9 @@ return [
         'bin' => 'vendor/bin/propel',
         'project' => 'push',
         'database' => 'pgsql',
-        'dsn' => 'pgsql:host=host.docker.internal;port=5432;dbname=push',
-        'db_user' => 'postgres',
-        'db_password' => 'root',
+        'dsn' => 'pgsql:host=PG_HOST;port=PG_PORT;dbname=PG_DATABASE',
+        'db_user' => 'PG_USER',
+        'db_password' => 'PG_PASSWORD',
         'platform' => 'pgsql',
         'config_dir' => 'src/Resource/propel/connection',
         'schema_dir' => 'src/Resource/propel/schema',
@@ -17,29 +17,29 @@ return [
 
     'push' => [
         'timezone' => 'Utc',
-        'chunk_size' => 500,
-        'engine' => 'gorush'
+        'chunk_size' => CHUNK_SIZE,
+        'engine' => 'PUSH_ENGINE'
     ],
 
     'gorush' => [
-        'host' => 'http://gorush:8088',
+        'host' => 'GORUSH_HOST',
     ],
 
     'queue' => [
-        'host' => 'http://queue'
+        'host' => 'QUEUE_HOST'
     ],
 
     'google' => [
-        'file' => 'google_prod.json',
-        'url' => 'https://fcm.googleapis.com/v1/projects/myproject-b5ae1/messages:send',
+        'file' => 'GOOGLE_FILE',
+        'url' => 'GOOGLE_URL',
     ],
     'apple' => [
-        'file' => 'dev.pem',
-        'url' => 'https://api.sendbox.push.apple.com:443/3/device/',
-        'bundle_id' => 'kz.naimi.app',
+        'file' => 'APPLE_FILE',
+        'url' => 'APPLE_URL',
+        'bundle_id' => 'APPLE_BUNDLE_ID',
     ],
     'huawei' => [
-        'file' => 'huawei_prod.json',
-        'url' => 'https://push-api.cloud.huawei.com/v1/%s/messages:send',
+        'file' => 'HUAWEI_FILE',
+        'url' => 'HUAWEI_URL',
     ],
 ];
--- project/src/Resource/propel/connection/propel.php
+++ project/src/Resource/propel/connection/propel.php
@@ -6,9 +6,9 @@ return [
             'connections' => [
                 'push' => [
                     'adapter' => 'pgsql',
-                    'dsn' => 'pgsql:host=host.docker.internal;port=5432;dbname=push',
-                    'user' => 'postgres',
-                    'password' => 'root',
+                    'dsn' => 'pgsql:host=PG_HOST;port=PG_PORT;dbname=PG_DATABASE',
+                    'user' => 'PG_USER',
+                    'password' => 'PG_PASSWORD',
                     'settings' => [
                         'charset' => 'utf8',
                         'queries' => [
